7f7751d41542e87893190ea6beb73fe570115b4c
Scylla <dogaerdemfani@Graf.local>
2025-10-05 16:15:25 +0200

    fix: Define Sequelize in model index

--- src/models/index.js
+++ src/models/index.js
@@ -1,23 +1,20 @@
 // src/models/index.js
+const { Sequelize } = require('sequelize'); // <-- EKSİK OLAN SATIR BUYDU
 const sequelize = require('../../config/database');
 const User = require('./user.model');
 const Market = require('./market.model');
 const Share = require('./share.model');
 
 // İlişkileri Tanımlama
-// Bir Kullanıcının birden çok hissesi olabilir
 User.hasMany(Share, { foreignKey: 'userId' });
-// Her Hisse bir Kullanıcıya aittir
 Share.belongsTo(User, { foreignKey: 'userId' });
 
-// Bir Pazarın birden çok hissesi olabilir
 Market.hasMany(Share, { foreignKey: 'marketId' });
-// Her Hisse bir Pazara aittir
 Share.belongsTo(Market, { foreignKey: 'marketId' });
 
 const db = {
   sequelize,
-  Sequelize,
+  Sequelize, // <-- BU SATIRIN ÇALIŞABİLMESİ İÇİN YUKARIDA IMPORT ETTİK
   User,
   Market,
   Share
