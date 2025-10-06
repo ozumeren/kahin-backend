c65336ccf7412b8207de3306052e14a17e291c85
Scylla <dogaerdemfani@Graf.local>
2025-10-06 11:06:20 +0200

    route fix

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.login);
+router.post('/register', authController.register);
 
 module.exports = router;
